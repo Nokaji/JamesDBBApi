71109288108c28b09d3fb0efe9740a7280647217
Nokaji <nokaji.dev@gmail.com>
2025-07-26 03:01:28 +0200

    Correction : Ajout d'une condition pour le paramètre hostname dans la configuration du serveur en production

--- index.ts
+++ index.ts
@@ -341,7 +341,7 @@ class App {
             {
                 fetch: this.app.fetch,
                 port,
-                ...(ConfigManager.isProduction() ? { hostname: host } : {})
+                ...(ConfigManager.isProduction() && host !== "0.0.0.0" ? { hostname: host } : {})
             },
             (info) => {
                 if (info) {
